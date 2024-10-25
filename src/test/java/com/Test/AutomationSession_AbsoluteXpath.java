c17c74e2d79e9300d7b7e048b65985a8355cef7e
GovindPatil75 <mulegr75@gmail.com>
2024-10-25 09:08:54 +0530

    Absolute Xpath session

---
+++ src/test/java/com/Test/AutomationSession_AbsoluteXpath.java
@@ -0,0 +1,33 @@
+package com.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutomationSession_AbsoluteXpath {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		//Element - Absolute Xpath (/)
+		// Navigation start parent node to immidate child node navigate -element 
+		
+		WebElement Email=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+		Email.sendKeys("Test@gmail.com");
+		
+		WebElement password=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"));
+		password.sendKeys("12345678");
+		
+		WebElement LoginBtn=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button"));
+		LoginBtn.click();
+		
+		
+	}
+
+}
